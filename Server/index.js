66ba6a92eed80b5f069fff449809ce9abe0f0496
terjr97 <47218086+terjr97@users.noreply.github.com>
2019-10-10 12:25:06 -0400

    Fix THE GAME CONTROLLER!

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const userController = require('./controllers/Users')
-const gameController = require('./controllers/Game')
+//const gameController = require('./controllers/Game')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +11,6 @@ const port = process.env.PORT || 3000;
         .get('/sql',  (req, res)=> res.send(process.env.MYSQLCONNSTR_localdb))
         .use('/static', express.static(path.join(__dirname, '../NoFrame')))
         .use('/users', userController )
-        .use('/game', gameController );
+//        .use('/game', gameController );
 
     app.listen(port, () => console.log(`Running on http://localhost:${port}`));
