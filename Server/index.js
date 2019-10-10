bc3cdc95f225ed79b3bd054eb02aa57a2760e164
terjr97 <47218086+terjr97@users.noreply.github.com>
2019-10-10 12:04:10 -0400

    We good?

--- Server/index.js
+++ Server/index.js
@@ -1,33 +1,17 @@
 const express = require('express');
 const path = require('path');
 const userController = require('./controllers/Users')
+const gameController = require('./controllers/Game')
 
 const app = express();
-const port = 3000;
+const test = true || SVGComponentTransferFunctionElement(44);
+const port = process.env.PORT || 3000;
 
     app
-
+        .get('/port', (req, res)=> res.send("Using Port: "+port))
+        .get('/sql',  (req, res)=> res.send(process.env.MYSQLCONNSTR_localdb))
         .use('/static', express.static(path.join(__dirname, '../NoFrame')))
-        .use(function(req, res, next){
-            // logging 
-
-            console.log({params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers});
-            next();
-        })
-        .get(    '/',
-             (req, res) => 
-                res.send('Hello Newpaltz!')
-            )
-        .get(   '/heb',
-             function(req, res){
-                    res.send('Shalom World!')
-            }
-         )
-         .get(   '/trial',
-             function(req, res){
-                    res.send({msg:'Shalom World!'})
-            }
-         )
-         .use('/Users', userController);
+        .use('/users', userController )
+        .use('/game', gameController );
 
     app.listen(port, () => console.log(`Running on http://localhost:${port}`));
