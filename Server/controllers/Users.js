73f9d52b38490733dd70d8bcc91df3da6c617536
terjr97 <47218086+terjr97@users.noreply.github.com>
2019-10-21 16:24:49 -0400

    working on dem changes

--- Server/controllers/Users.js
+++ Server/controllers/Users.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const users = require('../models/users')
-const app = express.Router()
+const users = require('../models/users');
+
+const app = express.Router();
 
 app.get('/', (req, res) => res.send(users))
 app.post('/', (req, res) => {
-    users.push(req.query)
-    res.send(users[users.length-1])
+    users.push(req.query);
+    res.send(users[users.length-1]);
+})
+
+app.get('/remove', (req, res) => res.send(users))
+app.post('/remove', (req, res) => {
+    users.splice(req.query);
+    res.send(users[users.length-1]);
+})
+
+app.get('/changeName', (req, res) => res.send(users))
+app.post('/changeName', (req, res) => {
+    users.splice(req.query);
+    users.push(req.query);
+    res.send(users[users.length-1]);
+})
+
+app.get('/changeAge', (req, res) => res.send(users))
+app.post('/changeAge', (req, res) => {
+    users.splice(req.query);
+    users.push(req.query);
+    res.send(users[users.length-1]);
 })
 
 module.exports = app;
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-//const userController = require('../controllers/Users')
+const userController = require('../Server/controllers/Users')
 //const gameController = require('../controllers/Game')
 
 const app = express();
@@ -10,7 +10,10 @@ const port = process.env.PORT || 3000;
         .get('/port', (req, res)=> res.send("Using Port: "+port))
         .get('/sql',  (req, res)=> res.send(process.env.MYSQLCONNSTR_localdb))
         .use('/static', express.static(path.join(__dirname, '../NoFrame')))
- //       .use('/users', userController )
+        .use('/users', userController )
+        .use('/users/remove', userController)
+        .use('/users/changeAge', userController)
+        .use('/users/changeName', userController)
 //        .use('/game', gameController );
 
     app.listen(port, () => console.log(`Running on http://localhost:${port}`));
